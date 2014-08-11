d1f3487b8762fd3e75ad3c441e0101f03cc2da2e
Niko <niko.nyrhila@gmail.com>
2014-08-12 00:30:29 +0300

    Initial AggregationQuery and tests, should confirm that queries are actually valid

---
+++ src/NikoNyrh/ElasticAggregator/AggregationQuery.php
@@ -0,0 +1,213 @@
+<?php
+namespace NikoNyrh\ElasticAggregator;
+
+class AggregationQuery
+{
+	protected $filters;
+	protected $aggregates;
+	protected $nestedPath;
+	
+	public function __construct()
+	{
+		$this->filters    = array();
+		$this->aggregates = array();
+		$this->nestedPath = array();
+	}
+	
+	public function genFilter($type, $config = null)
+	{
+		$passThroughTypes = array_flip(array(
+			'prefix',
+			'range',
+			'terms',
+			'term'
+		));
+		
+		$nestedPath = empty($this->nestedPath) ?
+			'' : (implode('.', $this->nestedPath) . '.');
+		
+		if (is_array($type)) {
+			return $type;
+		}
+		elseif (isset($passThroughTypes[$type])) {
+			if (is_array($config)) {
+				if (isset($config['field'])) {
+					$config = array($nestedPath . $config['field'] => $config['condition']);
+				}
+				elseif (sizeof($config) == 1 && !empty($nestedPath)) {
+					$field = key($config);
+					$config = array($nestedPath . $field => $config[$field]);
+				}
+			}
+			
+			return array(
+				$type => $config
+			);
+		}
+		elseif ($type == 'not') {
+			return array(
+				$type => $this->genFilter($config['type'], $config['condition'])
+			);
+		}
+		elseif ($type == 'or') {
+			foreach ($config as $key => $c) {
+				$config[$key] = $this->genFilter(
+					$c['type'],
+					isset($c['condition']) ? $c['condition']  : null
+				);
+			}
+			
+			return array(
+				$type => $config
+			);
+		}
+		elseif ($type == 'nested') {
+			// Push the path and get the filter
+			array_push($this->nestedPath, $config['path']);
+			$result = array(
+				$type => array(
+					'path' => $config['path'],
+					'filter' => $this->genFilter(
+						$config['type'],
+						$config['condition']
+					)
+				)
+			);
+			
+			// Pop the path and return the result
+			array_pop($this->nestedPath);
+			return $result;
+		}
+		else {
+			throw new \InvalidArgumentException("Unknown type '$type'!");
+		}
+	}
+	
+	public function filter($type, $config = null)
+	{
+		$this->filters[] = $this->genFilter($type, $config);
+		return $this;
+	}
+	
+	public function aggregate($type, $config = null)
+	{
+		$i = sizeof($this->aggregates) + 1;
+		
+		if (is_array($type)) {
+			$this->aggregates[$type['field'] . "_agg_$i"] = $type['aggs'];
+		}
+		elseif ($type == 'terms') {
+			$this->aggregates[$config . "_agg_$i"] = array(
+				$type => array('field' => $config)
+			);
+		}
+		elseif ($type == 'date_histogram') {
+			$this->aggregates[$config['field'] . "_agg_$i"] = array(
+				$type => array(
+					'field'         => $config['field'],
+					'interval'      => $config['interval'],
+					'min_doc_count' => isset($config['min_doc_count']) ?
+						$config['min_doc_count'] : 0
+				)
+			);
+		}
+		elseif ($type == 'histogram') {
+			$this->aggregates[$config['field'] . "_agg_$i"] = array(
+				$type => array(
+					'field'           => $config['field'],
+					'interval'        => $config['interval'],
+					'min_doc_count'   => isset($config['min_doc_count']) ?
+						$config['min_doc_count'] : 0,
+					'extended_bounds' => isset($config['extended_bounds']) ?
+						$config['extended_bounds'] : null
+				)
+			);
+		}
+		elseif ($type == 'filter') {
+			$this->aggregates[$config['field'] . "_filter_$i"] = array(
+				$type => array(
+					$config['subtype'] => array(
+						$config['field'] => $config['condition']
+					)
+				)
+			);
+		}
+		else {
+			throw new \InvalidArgumentException("Unknown type '$type'!");
+		}
+		
+		return $this;
+	}
+	
+	public function stats($fields)
+	{
+		if (!is_array($fields)) {
+			$fields = array($fields);
+		}
+		
+		$stats = array();
+		
+		foreach ($fields as $field) {
+			if (is_array($field)) {
+				$stats[$field['field'] . '_' . $field['type']] = array(
+					$field['type'] => array('field' => $field['field'])
+				);
+			}
+			else {
+				$stats[$field . '_stats'] = array(
+					'stats' => array('field' => $field)
+				);
+			}
+		}
+		
+		$i = sizeof($this->aggregates) + 1;
+		$this->aggregates["stats_$i"] = $stats;
+		
+		return $this;
+	}
+	
+	public function buildBody()
+	{
+		$aggs = array();
+		
+		// The most recently added aggregations go to the deepest nesting level
+		foreach (array_reverse(array_keys($this->aggregates)) as $key) {
+			$aggKey = preg_replace('/_[0-9]+$/', '', $key);
+			
+			if (empty($aggs)) {
+				$aggs = array($aggKey => $this->aggregates[$key]);
+			}
+			else {
+				$agg = $this->aggregates[$key];
+				$agg['aggs'] = $aggs;
+				$aggs = array($aggKey => $agg);
+			}
+		}
+		
+		$body = array(
+			'size' => 0,
+			'aggs' => $aggs
+		);
+		
+		if (!empty($this->filters)) {
+			if (sizeof($this->filters) == 1) {
+				// Single filter works on its own
+				$filter = $this->filters[0];
+			}
+			else {
+				// Multiple filters are combined by "and" filter
+				$filter = array(
+					'and' => $this->filters
+				);
+			}
+			
+			$body['query'] = array(
+				'filtered' => array(
+					'filter' => $filter
+				)
+			);
+		}
+		
+		return $body;
+	}
+}
---
+++ src/NikoNyrh/ElasticAggregator/Tests/AggregationQueryTest.php
@@ -0,0 +1,317 @@
+<?php
+namespace NikoNyrh\ElasticAggregator\Tests;
+
+class AggregationQueryTest extends \PHPUnit_Framework_TestCase
+{
+	protected function query()
+	{
+		return new \NikoNyrh\ElasticAggregator\AggregationQuery();
+	}
+	
+	protected function decode($json)
+	{
+		// Return an array, not an object
+		return json_decode($json, true);
+	}
+	
+	public function testBasicAggregation_1()
+	{
+		// - Find users with most posts,
+		// - group posts by date and
+		// - calculate stats on post lengths
+		$result = $this->query()
+			->aggregate('terms', 'users')
+			->aggregate('date_histogram', array(
+				'field'         => 'post_date',
+				'interval'      => '1d',
+				'min_doc_count' => 1
+			))
+			->stats('post_length')
+			->buildBody();
+		
+		$this->assertEquals($this->decode('
+			{
+				"size": 0,
+				"aggs": {
+					"users_agg": {
+						"terms": {
+							"field": "users"
+						},
+						"aggs": {
+							"post_date_agg": {
+								"date_histogram": {
+									"field": "post_date",
+									"interval": "1d",
+									"min_doc_count": 1
+								},
+								"aggs": {
+									"stats": {
+										"post_length_stats": {
+											"stats": {
+												"field": "post_length"
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		'), $result);
+	}
+	
+	public function testBasicAggregation_2()
+	{
+		// - Group posts by length and
+		// - filter on of posts tagged as ES and
+		// - get maximum post length and the average number of tags
+		$result = $this->query()
+			->aggregate('histogram', array(
+				'field'           => 'post_length',
+				'interval'        => 25,
+				'extended_bounds' => array(
+					'min' => 0,
+					'max' => 100
+				)
+			))
+			->aggregate('filter', array(
+				'field'     => 'tag',
+				'subtype'   => 'term',
+				'condition' => 'ES'
+			))
+			->stats(array(
+				array('type' => 'max', 'field' => 'post_length'),
+				array('type' => 'avg', 'field' => 'num_of_tags')))
+			->buildBody();
+		
+		$this->assertEquals($this->decode('
+			{
+				"size": 0,
+				"aggs": {
+					"post_length_agg": {
+						"histogram": {
+							"field": "post_length",
+							"interval": 25,
+							"min_doc_count": 0,
+							"extended_bounds": {
+								"min": 0,
+								"max": 100
+							}
+						},
+						"aggs": {
+							"tag_filter": {
+								"filter": {
+									"term": {
+										"tag": "ES"
+									}
+								},
+								"aggs": {
+									"stats": {
+										"post_length_max": {
+											"max": {
+												"field": "post_length"
+											}
+										},
+										"num_of_tags_avg": {
+											"avg": {
+												"field": "num_of_tags"
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		'), $result);
+	}
+	
+	public function testBasicFiltering_1()
+	{
+		// - Keep posts not tagged as ES or MySQL and
+		// - keep posts longer than 50 characters and
+		// - get users with most matchings posts
+		$result = $this->query()
+			->filter('not', array(
+				'type'      => 'terms',
+				'condition' => array('tag' => array('ES', 'MySQL'))
+			))
+			->filter('range', array(
+				'field'     => 'post_length',
+				'condition' => array('gt' => 50)
+			))
+			->aggregate('terms', 'users')
+			->buildBody();
+		
+		$this->assertEquals($this->decode('
+			{
+				"size": 0,
+				"aggs": {
+					"users_agg": {
+						"terms": {
+							"field": "users"
+						}
+					}
+				},
+				"query": {
+					"filtered": {
+						"filter": {
+							"and": [
+								{
+									"not": {
+										"terms": {
+											"tag": [
+												"ES",
+												"MySQL"
+											]
+										}
+									}
+								},
+								{
+									"range": {
+										"post_length": {
+											"gt": 50
+										}
+									}
+								}
+							]
+						}
+					}
+				}
+			}
+		'), $result);
+	}
+	
+	public function testBasicFiltering_2()
+	{
+		// - Keep posts not tagged as ES, or having ids 1, 2 or 3
+		// - get users with most matchings posts
+		$result = $this->query()
+			->filter('or', array(
+				array(
+					'type'      => 'term',
+					'condition' => array('tag' => 'ES')
+				),
+				array(
+					// This is an example usage if the desired filter is not "natively"
+					// implemented. Just put the whole thing to the 'type' element.
+					'type' => array(
+						'ids' => array(
+							'type'   => 'post',
+							'values' => array(1, 2, 3)
+						)
+					)
+				)
+			))
+			->aggregate('terms', 'users')
+			->buildBody();
+		
+		$this->assertEquals($this->decode('
+			{
+				"size": 0,
+				"aggs": {
+					"users_agg": {
+						"terms": {
+							"field": "users"
+						}
+					}
+				},
+				"query": {
+					"filtered": {
+						"filter": {
+							"or": [
+								{
+									"term": {
+										"tag": "ES"
+									}
+								},
+								{
+									"ids": {
+										"type": "post",
+										"values": [
+											1,
+											2,
+											3
+										]
+									}
+								}
+							]
+						}
+					}
+				}
+			}
+		'), $result);
+	}
+	
+	public function testBasicFiltering_3()
+	{
+		$result = $this->query()
+			->filter('nested', array(
+				'path'      => 'link',
+				'type'      => 'prefix',
+				'condition' => array('url' => 'www.google.com/')
+			))
+			->aggregate(array(
+				'field' => 'user',
+				'aggs'  => array(
+					'missing' => array(
+						'field' => 'link'
+					)
+				)
+			))
+			->aggregate('terms', 'users')
+			->buildBody();
+		
+		$this->assertEquals($this->decode('
+			{
+				"size": 0,
+				"aggs": {
+					"user_agg": {
+						"missing": {
+							"field": "link"
+						},
+						"aggs": {
+							"users_agg": {
+								"terms": {
+									"field": "users"
+								}
+							}
+						}
+					}
+				},
+				"query": {
+					"filtered": {
+						"filter": {
+							"nested": {
+								"path": "link",
+								"filter": {
+									"prefix": {
+										"link.url": "www.google.com/"
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		'), $result);
+	}
+	
+	/**
+	* @expectedException InvalidArgumentException
+	*/
+	public function testInvalidFilterType()
+	{
+		$result = $this->query()->filter('no_such_filter');
+	}
+	
+	/**
+	* @expectedException InvalidArgumentException
+	*/
+	public function testInvalidAggregationType()
+	{
+		$result = $this->query()->aggregate('no_such_filter');
+	}
+}
